830e4ad05cf6232ebc277c6a104c73d15828991e
Naoki Kubokawa <garigarireorio@yahoo.co.jp>
2019-02-14 01:48:14 +0900

    特定の環境でPythonShellのsendでエラーになるのを修正

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const {PythonShell} = require('python-shell');
 const {ipcMain} = require('electron');
 // IPC通信のレンダラープロセスからのリクエストに対してレスポンスを返す。
 ipcMain.on('push-button', (event, arg) => {
-    var pyshell = new PythonShell('./python/test.py');
+    var pyshell = new PythonShell('./python/test.py',{pythonPath : 'python'});
     pyshell.send(arg.param);
     pyshell.on('message', function (data) {
         console.log('response: ' + data);
